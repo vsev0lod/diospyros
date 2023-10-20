8ab3b44e14b81520a89294743c45958fc18d3651
vsev0lod <hi.upper@gmail.com>
2023-10-20 19:19:48 +0200

    Models for Report and Taks

--- backend/src/main/java/com/diospyros/uplift/model/Report.java
+++ backend/src/main/java/com/diospyros/uplift/model/Report.java
@@ -1,5 +1,13 @@
 package com.diospyros.uplift.model;
 
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
 public class Report {
     // Java model to store Reports
     // it should contain Photo, comment and executor data
@@ -8,5 +16,13 @@ public class Report {
     // Executor - user who completed the task
     // Comment - comment of the completed task
 
+    int id;
+    Photo photo;
+    User executor;
+    String comment;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    Date createdAt;
 
 }
---
+++ backend/src/main/java/com/diospyros/uplift/model/Task.java
@@ -0,0 +1,44 @@
+package com.diospyros.uplift.model;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Entity
+@Value
+@AllArgsConstructor
+public class Task {
+    // Java model to store Tasks
+    // it should contain task id, author id, photo, description, location, reward, status(enum), executor id, report id, created at
+    // id - id of the task
+    //  authorId - id of the author
+    //  photo - photo of the task
+    //  description - description of the task
+    //  location - location of the task
+    //  reward - reward of the task
+    //  status - status of the task - enum (created, in progress, completed)
+    //  executorId - id of the executor
+    //  reportId - id of the report
+    //  createdAt - date of creation of the task
+
+    int id;
+    int authorId;
+    Photo photo;
+    String description;
+    String location;
+    int reward;
+    String status;
+    int executorId;
+    int reportId;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    Date createdAt;
+}
