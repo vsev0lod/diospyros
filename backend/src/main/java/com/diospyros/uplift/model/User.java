750f9b960567a4e2272a5177fcd4f3270b27f87f
Andrei Temnikov <temnikov.andrey1@gmail.com>
2023-10-20 17:33:45 +0200

    Adjust user model

--- backend/src/main/java/com/diospyros/uplift/model/User.java
+++ backend/src/main/java/com/diospyros/uplift/model/User.java
@@ -1,9 +1,15 @@
 package com.diospyros.uplift.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Value;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Value
@@ -14,5 +20,9 @@ public class User {
     String username;
     String email;
     Long avatarId;
-    double balance;
+    Long balance;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    Date createdAt;
 }
