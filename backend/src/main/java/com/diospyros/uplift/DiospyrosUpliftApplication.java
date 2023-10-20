89bbd6e30b3c33b53d7dc8df20ee526fb05fc68d
Andrei Temnikov <temnikov.andrey1@gmail.com>
2023-10-20 14:31:15 +0200

    init backend

---
+++ backend/src/main/java/com/diospyros/uplift/DiospyrosUpliftApplication.java
@@ -0,0 +1,13 @@
+package com.diospyros.uplift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiospyrosUpliftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiospyrosUpliftApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diospyros/uplift/DiospyrosUpliftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diospyros.uplift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiospyrosUpliftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
