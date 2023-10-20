73c178365eef53a35b9c74fa292cd7cf2c567f28
vsev0lod <hi.upper@gmail.com>
2023-10-20 19:14:56 +0200

    init report

---
+++ backend/src/main/java/com/diospyros/uplift/model/Report.java
@@ -0,0 +1,12 @@
+package com.diospyros.uplift.model;
+
+public class Report {
+    // Java model to store Reports
+    // it should contain Photo, comment and executor data
+    // id - id of the report
+    // Photo - photo of the completed task
+    // Executor - user who completed the task
+    // Comment - comment of the completed task
+
+
+}
