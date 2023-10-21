68da86498f8682b83e2774036a471782d4a3578a
dronky <dronskiy95@yandex.ru>
2023-10-21 14:36:11 +0200

    added flyway

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # diospyros
 Collabothon 2023 - Diospyros
+
+
+Local DB:
+docker pull postgres:15.4
+#docker run --name diospyros_local -e POSTGRES_PASSWORD=diospyros -d postgres
+docker run -d --name diospyros_local -p 5432:5432 -e POSTGRES_PASSWORD=diospyros postgres:15.4
+docker exec -it diospyros_local /bin/bash
+psql -U postgres
+
+# different user:
+CREATE USER diospyros;
+ALTER USER diospyros WITH PASSWORD 'diospyros';
+CREATE DATABASE diospyros;
+GRANT ALL PRIVILEGES ON DATABASE diospyros TO diospyros;
+GRANT CREATE ON SCHEMA public TO diospyros;
+ALTER DATABASE diospyros OWNER TO diospyros;
+
+#using postgres:
+GRANT ALL PRIVILEGES ON DATABASE diospyros TO postgres;
+
+migration loc:
+/Users/andrey/IdeaProjects/diospyros/backend/src/main/resources/db/migration
---
+++ backend/src/main/resources/db/migration/V1_1_0__initialdb.sql
@@ -0,0 +1,317 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.4
+-- Dumped by pg_dump version 15.4 (Debian 15.4-2.pgdg110+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: avatars; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.avatars (
+    avatar_id integer NOT NULL,
+    image text
+);
+
+
+ALTER TABLE public.avatars OWNER TO postgres;
+
+--
+-- Name: avatars_avatar_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.avatars_avatar_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.avatars_avatar_id_seq OWNER TO postgres;
+
+--
+-- Name: avatars_avatar_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.avatars_avatar_id_seq OWNED BY public.avatars.avatar_id;
+
+--
+-- Name: task; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.task (
+    task_id integer NOT NULL,
+    photo text,
+    desk character varying(255) NOT NULL,
+    location character varying(255) NOT NULL,
+    prize character varying(100) NOT NULL,
+    status text,
+    FOREIGN KEY (executor_id) REFERENCES user(user_id),
+    FOREIGN KEY (report_id) REFERENCES id(user_id),
+    FOREIGN KEY (author_id) REFERENCES user(user_id)
+);
+
+
+ALTER TABLE public.avatars OWNER TO postgres;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    username character varying(50) NOT NULL,
+    email character varying(255) NOT NULL,
+    created_on timestamp without time zone NOT NULL,
+    avatar_id integer,
+    balance integer
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_user_id_seq OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: avatars avatar_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.avatars ALTER COLUMN avatar_id SET DEFAULT nextval('public.avatars_avatar_id_seq'::regclass);
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: avatars; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.avatars (avatar_id, image) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, username, email, created_on, avatar_id, balance) FROM stdin;
+\.
+
+
+--
+-- Name: avatars_avatar_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.avatars_avatar_id_seq', 1, false);
+
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+
+
+--
+-- Name: avatars avatars_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.avatars
+    ADD CONSTRAINT avatars_pkey PRIMARY KEY (avatar_id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_key UNIQUE (username);
+
+
+--
+-- Name: users fk_avatart; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT fk_avatart FOREIGN KEY (avatar_id) REFERENCES public.avatars(avatar_id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: pg_database_owner
+--
+
+GRANT ALL ON SCHEMA public TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_advance(text, pg_lsn); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_advance(text, pg_lsn) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_create(text); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_create(text) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_drop(text); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_drop(text) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_oid(text); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_oid(text) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_progress(text, boolean); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_progress(text, boolean) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_session_is_setup(); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_session_is_setup() TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_session_progress(boolean); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_session_progress(boolean) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_session_reset(); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_session_reset() TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_session_setup(text); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_session_setup(text) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_xact_reset(); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_xact_reset() TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_replication_origin_xact_setup(pg_lsn, timestamp with time zone); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_replication_origin_xact_setup(pg_lsn, timestamp with time zone) TO cloudsqlsuperuser;
+
+
+--
+-- Name: FUNCTION pg_show_replication_origin_status(OUT local_id oid, OUT external_id text, OUT remote_lsn pg_lsn, OUT local_lsn pg_lsn); Type: ACL; Schema: pg_catalog; Owner: cloudsqladmin
+--
+
+GRANT ALL ON FUNCTION pg_catalog.pg_show_replication_origin_status(OUT local_id oid, OUT external_id text, OUT remote_lsn pg_lsn, OUT local_lsn pg_lsn) TO cloudsqlsuperuser;
+
+CREATE TYPE status AS ENUM (
+    'added',
+    'checked',
+    'in progress',
+    'completed'
+);
+
+CREATE TABLE task (
+    task_id integer PRIMARY KEY NOT NULL,
+    photo TEXT,
+    desk VARCHAR(255) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    reward VARCHAR(100),
+    status,
+    FOREIGN KEY (executor_id) REFERENCES user(user_id),
+    FOREIGN KEY (report_id) REFERENCES id(user_id),
+    FOREIGN KEY (author_id) REFERENCES user(user_id)
+);
+
+
+
+ALTER TABLE avatar RENAME TO photos;
+ALTER TABLE photos RENAME COLUMN avatar_id TO photos_id;
+ALTER TABLE users RENAME COLUMN avatar_id TO photos_id;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
